9b1c9d30a3c0966066e5c0d809c463ce170a1e92
Max Beier <max@maxbeier.dev>
2023-09-15 11:19:46 +0000

    naming

--- MoodleMessage/__init__.py
+++ MoodleMessage/__init__.py
@@ -4,14 +4,24 @@ from MoodleMessage.send import send_text
 class MoodleMessage:
     hostname = ""
     session_token = ""
+    conversation_id = ""
 
-    def __init__(self, hostname, session_token) -> None:
+    def __init__(self, hostname, session_token, conversation_id) -> None:
         self.hostname = hostname
         self.session_token = session_token
+        self.conversation_id = conversation_id
 
-    def send_message(self, text, conversation_id):
-        send_text(text, conversation_id, self.hostname, self.session_token)
+    def send_message(self, text):
+        send_text(text, self.conversation_id, self.hostname, self.session_token)
 
-    def send_image(self, image_url, conversation_id):
+    def send_image(self, image_url):
         text = f'<img src="{image_url}"/>'
-        send_text(text, conversation_id, self.hostname, self.session_token)
+        send_text(text, self.conversation_id, self.hostname, self.session_token)
+
+    def send_text_array(self, text_array):
+        for text in text_array:
+            pass
+
+    def send_image_array(self, image_url_array):
+        for url in image_url_array:
+            pass
